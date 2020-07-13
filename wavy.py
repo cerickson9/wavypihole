6d9082e24c6fd1e3985910703149e531e8ba74b9
umbrellathorn <umbrellathorn@gmail.com>
2020-07-13 18:17:27 -0400

    learning python lol

--- wavy.py
+++ wavy.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(1, "./drivers") # Adds lib folder in this directory to sys
+sys.path.insert(1, "./lib") # Adds lib folder in this directory to sys
 import epd2in13_V2
 from PIL import Image, ImageDraw, ImageFont
 

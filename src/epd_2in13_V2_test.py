b5492d3bce39fdcacca4cfde1b4b19efc4061a13
umbrellathorn <umbrellathorn@gmail.com>
2020-07-13 23:40:51 -0400

    hello world v2

--- src/epd_2in13_V2_test.py
+++ src/epd_2in13_V2_test.py
@@ -24,13 +24,11 @@ white = 1
 black = 0
 
 def printToDisplay(string):
-    image = Image.open(os.path.join(picdir, 'logo.png'))
+    image = Image.open(os.path.join(picdir, 'logo.bpm'))
     font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 22)
     draw = ImageDraw.Draw(image)
-
-    while (True):
-        draw.text((25, 30), string, font = font, fill = white)
-        epd.display(epd.getbuffer(image))
+    draw.text((25, 30), string, font = font, fill = white)
+    # epd.display(epd.getbuffer(image))
 
 msg = "Hello Jeff!"
 printToDisplay(msg)

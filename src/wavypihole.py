316224435656b81816ba2e412f4f4dc41ea582ad
umbrellathorn <umbrellathorn@gmail.com>
2020-07-15 00:19:01 -0400

    json debugging

--- src/wavypihole.py
+++ src/wavypihole.py
@@ -53,7 +53,7 @@ def printToDisplay():
     draw.text((25, 20), str(adsblocked), font = font, fill = black) 
     draw.text((25, 50), "font example", font = font, fill = black) 
 
-    draw.text((25, 50), str("%.1f" % round(ratioblocked,2)) + "%", font = font, fill = black) 
+    # draw.text((25, 50), str("%.1f" % round(ratioblocked,2)) + "%", font = font, fill = black) 
     epd.display(epd.getbuffer(image1))
 
 printToDisplay()

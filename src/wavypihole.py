19c73fce1317789fa94f0ebab26d7120f1236d87
umbrellathorn <umbrellathorn@gmail.com>
2020-07-15 01:32:36 -0400

    json debugging

--- src/wavypihole.py
+++ src/wavypihole.py
@@ -52,7 +52,7 @@ try:
     logging.debug(parsed_json.keys())
     logging.debug(type(parsed_json))
     adsblocked = parsed_json.get(u"ads_blocked_today")
-    
+    logging.debug(adsblocked)
     # ratioblocked = parsed_json[u"ads_percentage_today"]
     f.close()
     # printToDisplay()
